a2f083ce955085055bdb166497712cf5e78fecdc
imostafao <138694850+imostafao@users.noreply.github.com>
2023-11-12 22:13:25 +0200

    Create str.funcs1.c

---
+++ str.funcs1.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * copyString - copies a string
+ * @destination: the destination
+ * @source: the source
+ *
+ * Return: pointer to the destination
+ */
+char *copyString(char *destination, char *source)
+{
+	int i = 0;
+
+	if (destination == source || source == NULL)
+		return destination;
+	while (source[i])
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = '\0';
+	return destination;
+}
+
+/**
+ * duplicateString - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *duplicateString(const char *str)
+{
+	int length  = 0;
+	char *ret;
+
+	if (str == NULL)
+		return NULL;
+	while (*str++)
+		length ++;
+	ret = malloc(sizeof(char) * (length  + 1));
+	if (!ret)
+		return NULL;
+	for (len++; length --;)
+		ret[length ] = *--str;
+	return ret;
+}
+
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - writes the character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1. On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUFFER_SIZE];
+
+	if (c == FLUSH_BUFFER || counter >= WRITE_BUFFER_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != FLUSH_BUFFER)
+		buf[i++] = c;
+	return 1;
+}
