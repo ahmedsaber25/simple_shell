0ad58bb32cd0e9d4a7c690a1c4512f285ebe4df5
ahmedsaber25 <138696889+ahmedsaber25@users.noreply.github.com>
2023-11-13 19:37:22 +0200

    Update chain.c

--- chain.c
+++ chain.c
@@ -14,23 +14,23 @@ int is_command_chain(info_t *info, char *buf, size_t *p)
 
     if (buf[j] == '|' && buf[j + 1] == '|') 
     {
-        buf[j] = 0;
-        j++;
-        info->command_buffer_type = COMMAND_OR;
+    buf[j] = 0;
+    j++;
+    info->command_buffer_type = COMMAND_OR;
     } 
     else if (buf[j] == '&' && buf[j + 1] == '&') 
     {
-        buffer[j] = 0;
-        j++;
-        info->command_buffer_type = COMMAND_AND;
+    buffer[j] = 0;
+    j++;
+    info->command_buffer_type = COMMAND_AND;
     } 
     else if (buf[j] == ';') { /* found the end of this command */ 
     {
-        buf[j] = 0; /* replace semicolon with null */
-        info->command_buffer_type = COMMAND_CHAIN;
+    buf[j] = 0; /* replace semicolon with null */
+    info->command_buffer_type = COMMAND_CHAIN;
     } 
     else 
-        return (0);
+    return (0);
     *p = j;
     return (1);
 }
@@ -53,20 +53,19 @@ void check_command_chain(info_t *info, char *buf, size_t *p, size_t i, size_t le
     {
         if (info->status) 
         {
-            buf[i] = 0;
-            j = len;
+        buf[i] = 0;
+        j = len;
         }
     }
     if (info->command_buffer_type == COMMAND_OR) 
     {
         if (!info->status) 
         {
-            buf[i] = 0;
-            j = len;
+        buf[i] = 0;
+        j = len;
         }
     }
-    
-        *p = j;
+    *p = j;
 }
 
 /**
@@ -85,14 +84,14 @@ int replace_shell_alias(info_t *info)
     {
         node = find_node_starts_with(info->alias, info->argv[0], '=');
         if (!node)
-            return (0);
+        return (0);
         free(info->argv[0]);
         p = findCharacter(node->str, '=');
         if (!p)
-            return (0);
+        return (0);
         p = duplicateString (p + 1);
         if (!p)
-            return (0);
+        return (0);
         info->argv[0] = p;
     }
     return (1);
@@ -112,23 +111,23 @@ int replace_variables(info_t *info)
     for (i = 0; info->argv[i]; i++) 
     {
         if (info->argv[i][0] != '$' || !info->argv[i][1])
-            continue;
+        continue;
 
         if (!stringCompare(info->argv[i], "$?")) 
         {
-            replace_string(&(info->argv[i]), duplicate_string(convertToText (info->status, 10, 0)));
-            continue;
+        replace_string(&(info->argv[i]), duplicate_string(convertToText (info->status, 10, 0)));
+        continue;
         }
         if (!stringCompare(info->argv[i], "$$")) 
         {
-            replace_string(&(info->argv[i]), duplicate_string(convertToText (get_process_id(), 10, 0)));
-            continue;
+        replace_string(&(info->argv[i]), duplicate_string(convertToText (get_process_id(), 10, 0)));
+        continue;
         }
         node = find_node_starts_with(info->env, &info->argv[i][1], '=');
         if (node) 
         {
-            replace_string(&(info->argv[i]), duplicate_string(findCharacter(node->str, '=') + 1));
-            continue;
+        replace_string(&(info->argv[i]), duplicate_string(findCharacter(node->str, '=') + 1));
+        continue;
         }
         replace_string(&info->argv[i], duplicate_string(""));
     }
@@ -144,7 +143,7 @@ int replace_variables(info_t *info)
  */
 int replace_string(char **old_string, char *new_string)
 {
-    free(*old_string);
-    *old_string = new_string;
-    return (1);
+free(*old_string);
+*old_string = new_string;
+return (1);
 }
