2c2986367857887bc37ed7fa6658fa2ead9f97eb
ahmedsaber25 <138696889+ahmedsaber25@users.noreply.github.com>
2023-11-13 17:41:54 +0200

    Update getline.c

--- getline.c
+++ getline.c
@@ -10,38 +10,38 @@
  */
 ssize_t receive_user_input_buffer(info_t *info, char **buf, size_t *len)
 {
-	ssize_t bytes_read = 0;
-	size_t length_position = 0;
+    ssize_t bytes_read = 0;
+    size_t length_position = 0;
 
-	if (!*len)/* if nothing left in the buffer, fill it */
-	{
-		/*bfree((void **)info->cmd_buf);*/
-		free(*buf);
-		*buf = NULL;
-		signal(SIGINT, handle_interrupt);
+    if (!*len) /* if nothing left in the buffer, fill it */
+    {
+        /*bfree((void **)info->cmd_buf);*/
+        free(*buf);
+        *buf = NULL;
+        signal(SIGINT, handle_interrupt);
 #if USE_GETLINE
-		bytes_read = getline(buf, &length_position, stdin);
+        bytes_read = getline(buf, &length_position, stdin);
 #else
-		bytes_read = get_line(info, buf, &length_position);
+        bytes_read = get_line(info, buf, &length_position);
 #endif
-	if (bytes_read > 0) 
-	{
-		if ((*buf)[bytes_read - 1] == '\n') 
-	        {
-		  (*buf)[bytes_read - 1] = '\0'; /* remove trailing newline */
-		  bytes_read--;
-                }
-	        info->linecount_flag = 1;
-	        eliminateComments(*buf);
-	        create_user_history_list(info, *buf, info->histcount++);
-	        /* if (findCharacter(*buf, ';')) is this a command chain? */
-	        {
-		  *len = bytes_read;
-	          info->cmd_buf = buf;
-	        }
-	}
-	}
-	return (bytes_read);
+        if (bytes_read > 0)
+        {
+            if ((*buf)[bytes_read - 1] == '\n')
+            {
+                (*buf)[bytes_read - 1] = '\0'; /* remove trailing newline */
+                bytes_read--;
+            }
+            info->linecount_flag = 1;
+            eliminateComments(*buf);
+            create_user_history_list(info, *buf, info->histcount++);
+            /* if (findCharacter(*buf, ';')) is this a command chain? */
+            {
+                *len = bytes_read;
+                info->cmd_buf = buf;
+            }
+        }
+    }
+    return (bytes_read);
 }
 
 /**
