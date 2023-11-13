3b9a136b7d4ddda7a82f1b4e38f23398db16e8e0
ahmedsaber25 <138696889+ahmedsaber25@users.noreply.github.com>
2023-11-13 19:29:02 +0200

    Update file_io_functions.c

--- file_io_functions.c
+++ file_io_functions.c
@@ -12,10 +12,10 @@ char *acquire_user_history_file(info_t *info)
 
     directory = _getenv(info, "HOME=");
     if (!directory)
-        return (NULL);
+    return (NULL);
     buf = malloc(sizeof(char) * (stringLength(directory) + stringLength(HISTORY_FILENAME) + 2));
     if (!buf)
-        return (NULL);
+    return (NULL);
     buf[0] = 0;
    copyString(buf, directory);
     stringConcat(buf, "/");
@@ -36,7 +36,7 @@ int write_history (info_t *info)
     list_t *node = NULL;
 
     if (!file_name)
-        return (-1);
+    return (-1);
 
     fd = open(file_name, O_CREAT | O_TRUNC | O_RDWR, 0644);
     free(file_name);
@@ -44,8 +44,8 @@ int write_history (info_t *info)
         return (-1);
     for (node = info->user_history; node; node = node->next)
     {
-        _putsfd(node->str, fd);
-        _putfd('\n', fd);
+    _putsfd(node->str, fd);
+    _putfd('\n', fd);
     }
     _putfd(FLUSH_BUFFER, fd);
     close(fd);
@@ -66,37 +66,37 @@ int read_history(info_t *info)
     char *buf = NULL, *file_name = acquire_user_history_file(info);
 
     if (!file_name)
-        return (0);
+    return (0);
 
     fd = open(file_name, O_RDONLY);
     free(file_name);
     if (fd == -1)
-        return (0);
+    return (0);
     if (!fstat(fd, &st))
-        file_size = st.st_size;
+    file_size = st.st_size;
     if (file_size < 2)
-        return (0);
+    return (0);
     buffer = malloc(sizeof(char) * (file_size + 1));
     if (!buf)
-        return (0);
+    return (0);
      rdlen = read(fd, buf, file_size);
     buf[file_size] = 0;
     if (rdlen <= 0)
-        return (free(buf), 0);
+    return (free(buf), 0);
     close(fd);
     for (i = 0; i < file_size; i++)
-        if (buf[i] == '\n')
-        {
-            buf[i] = 0;
-            create_user_history_list(info, buf + last, linecount++);
-            last = i + 1;
-        }
+    if (buf[i] == '\n')
+    {
+    buf[i] = 0;
+    create_user_history_list(info, buf + last, linecount++);
+    last = i + 1;
+    }
     if (last != i)
-        create_user_history_list(info, buf + last, linecount++);
+    create_user_history_list(info, buf + last, linecount++);
     free(buf);
     info->histcount = linecount;
     while (info->histcount-- >= HISTORY_MAXIMUM)
-        remove_node_at_index(&(info->user_history), 0);
+    remove_node_at_index(&(info->user_history), 0);
     update_user_history_numbers(info);
     return (info->histcount);
 }
@@ -114,11 +114,11 @@ int create_user_history_list(info_t *info, char *buf, int linecount)
     list_t *node = NULL;
 
     if (info->user_history)
-        node = info->user_history;
+    node = info->user_history;
     insert_node_end(&node, buf, linecount);
     
     if (!info->user_history)
-        info->user_history = node;
+    info->user_history = node;
     return (0);
 }
 
@@ -135,8 +135,8 @@ int update_user_history_numbers(info_t *info)
 
     while (node)
     {
-        node->num = i++;
-        node = node->next;
+    node->num = i++;
+    node = node->next;
     }
     return (info->histcount = i);
 }
