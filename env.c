fa33f17d383496765ee6f53af82a4250e518ea1d
ahmedsaber25 <138696889+ahmedsaber25@users.noreply.github.com>
2023-11-13 12:40:58 +0200

    Update env.c

--- env.c
+++ env.c
@@ -9,7 +9,7 @@
 int _myenv(info_t *info) 
 {
    print_list_data(info->env);
-    return 0;
+    return (0);
 }
 
 /**
@@ -28,10 +28,10 @@ char *_getenv(info_t *info, const char *name)
     {
         p = startsWith(node->str, name);
         if (p && *p)
-            return p;
+            return (p);
         node = node->next;
     }
-    return NULL;
+    return (NULL);
 }
 
 /**
@@ -46,11 +46,11 @@ int _mysetenv(info_t *info)
     if (info->argc != 3) 
     {
         _eputs("Incorrect number of arguments\n");
-        return 1;
+        return (1);
     }
     if (_setenv(info, info->argv[1], info->argv[2]))
-        return 0;
-    return 1;
+        return (0);
+    return (10;
 }
 
 /**
@@ -66,12 +66,12 @@ int _myunsetenv(info_t *info)
     if (info->argc == 1) 
     {
         _eputs("Too few arguments.\n");
-        return 1;
+        return (1);
     }
     for (i = 1; i <= info->argc; i++)
         _unsetenv(info, info->argv[i]);
 
-    return 0;
+    return (0);
 }
 
 /**
@@ -88,5 +88,5 @@ int populate_env_list(info_t *info)
     for (i = 0; environ[i]; i++)
         insert_node_end(&node, environ[i], 0);
     info->env = node;
-    return 0;
+    return (0);
 }
